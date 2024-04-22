6741813e84d0d45ab32ab000d700d0c476cf2910
Jasmyre <lanuzajasmyreandrei@gmail.com>
2024-04-22 11:56:51 +0800

    Added range prompt

--- src/App.java
+++ src/App.java
@@ -1,48 +1,48 @@
 import javax.swing.JOptionPane;
-import javax.swing.UIManager;
 
 public class App {
     public static void main(String[] args) {
-        
-
-        int range = 100;
-        double number;
-        boolean playAgain = true;
-
         do {
-            number = Math.ceil(Math.random() * range);
-            System.out.println("Number: " + number);
+            int range = Integer.parseInt(JOptionPane.showInputDialog("Please enter a range"));
+            double number;
+            boolean playAgain = true;
 
             do {
-                String guessSTR = JOptionPane.showInputDialog("Enter a guess from 1 - 100:");
-                if (guessSTR == null) {
-                    break;
-                }
+                number = Math.ceil(Math.random() * range);
+                System.out.println("Number: " + number);
     
-                if (guessSTR.length() > 0) {
-                    int guess = Integer.parseInt(guessSTR);
-    
-                    try {
-                        if (guess > number) {
-                            JOptionPane.showMessageDialog(null, "The guess is higher than the number.");
-                        } else if (guess < number) {
-                            JOptionPane.showMessageDialog(null, "The guess is lower than the number.");
-                        } else if (number == guess) {
-                            JOptionPane.showMessageDialog(null, "You guessed the NUMBER!!");
-                            
-                            break;
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Invalid Input");
-                        }
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                        JOptionPane.showMessageDialog(null, "Please enter a valid Number!");
+                do {
+                    String guessSTR = JOptionPane.showInputDialog("Enter a guess from 1 - " + range + ":");
+                    if (guessSTR == null) {
+                        break;
                     }
-                }
-            } while (playAgain);
+        
+                    if (guessSTR.length() > 0) {
+                        int guess = Integer.parseInt(guessSTR);
+        
+                        try {
+                            if (guess > number) {
+                                JOptionPane.showMessageDialog(null, "The guess is higher than the number.");
+                            } else if (guess < number) {
+                                JOptionPane.showMessageDialog(null, "The guess is lower than the number.");
+                            } else if (number == guess) {
+                                JOptionPane.showMessageDialog(null, "You guessed the NUMBER!!");
+                                
+                                break;
+                            } else {
+                                JOptionPane.showMessageDialog(null, "Invalid Input");
+                            }
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                            JOptionPane.showMessageDialog(null, "Please enter a valid Number!");
+                        }
+                    }
+                } while (playAgain);
+    
+            } while (false);
 
             int playAgainPrompt = JOptionPane.showConfirmDialog(null, "Do you want to continue playing?", "Play again?", JOptionPane.YES_NO_OPTION);
-
+    
             if (playAgainPrompt == 0) {
                 continue;
             } else {
@@ -50,7 +50,6 @@ public class App {
             }
 
         } while (true);
-        UIManager.put("OptionPane.minimumSize",new Dimension(750,1000)); 
         
     }
 }
